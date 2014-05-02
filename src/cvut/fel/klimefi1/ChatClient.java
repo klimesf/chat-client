52b32e19ceca3beddc30e0b3d78433ede65a916f
Filip Klimes <filip@filipklimes.cz>
2014-05-02 19:40:03 +0200

    Initial commit

---
+++ src/cvut/fel/klimefi1/ChatClient.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cvut.fel.klimefi1;
+
+/**
+ * Chat Client
+ * 
+ * Connects to a chat server and enables communication. User of the client can
+ * choose his/her nickname, retrieve a list of rooms on the server, create a new 
+ * room, enter an existing room, leave a room he entered and send messages
+ * within an entered room.
+ * 
+ * @author Filip Klimes <klimefi1@fel.cvut.cz>
+ */
+public class ChatClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
